fe84aec6ed1efd850b349acfa998e162aac97fb9
faustbrian <faustbrian@users.noreply.github.com>
2023-04-01 14:24:19 +0000

    chore: resolve style guide violations

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace PreemStudio\BladeIcons\IcoMoonEssential;
 
 use Illuminate\Support\Facades\File;
-use PreemStudio\BladeIcons\IconFamily;
 use PreemStudio\BladeIcons\Facades\IconFamilyRegistry;
+use PreemStudio\BladeIcons\IconFamily;
 use PreemStudio\Jetpack\Package\AbstractServiceProvider;
 
 final class ServiceProvider extends AbstractServiceProvider
