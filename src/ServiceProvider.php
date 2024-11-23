e97961081401a9b04d9fbd90c1240be53d93078d
Brian Faust <hello@basecode.sh>
2024-11-23 17:28:05 +0200

    first commit
    Signed-off-by: Brian Faust <hello@basecode.sh>

---
+++ src/ServiceProvider.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+
+/**
+ * Copyright (C) BaseCode Oy - All Rights Reserved
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace BaseCodeOy\BladeIcons\IcoMoonEssential;
+
+use BaseCodeOy\BladeIcons\Facades\IconFamilyRegistry;
+use BaseCodeOy\BladeIcons\IconFamily;
+use BaseCodeOy\PackagePowerPack\Package\AbstractServiceProvider;
+use Illuminate\Support\Facades\File;
+
+final class ServiceProvider extends AbstractServiceProvider
+{
+    public function packageRegistered(): void
+    {
+        $directory = resource_path('blade-icons/icomoon-essential');
+
+        if (File::exists($directory)) {
+            IconFamilyRegistry::push(IconFamily::fromDirectory('icomoon-essential', $directory));
+        }
+    }
+}
